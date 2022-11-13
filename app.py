91981ce3cca375259714d4ed43c59bc4151bea83
akask30 <117513122+akask30@users.noreply.github.com>
2022-11-13 19:03:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,17 @@ import sklearn
 from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
 model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
-@app.route('/',methods=['GET'])
-def Home():
+@app.route('/')
+def index():
     return render_template('index1.html')
 
+@app.route('/hai')
+def Hai():
+    return render_template('index.html')
+
+@app.route('/hello',methods=['GET'])
+def Home():
+    return render_template('index.html')
 
 standard_to = StandardScaler()
 @app.route("/predict", methods=['POST'])
@@ -43,12 +50,11 @@ def predict():
         prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
         output=round(prediction[0],2)
         if output<0:
-            return render_template('index1.html',prediction_texts="Sorry you cannot sell this car")
+            return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
         else:
-            return render_template('index1.html',prediction_text="You Can Sell The Car at {}".format(output))
+            return render_template('index.html',prediction_text="You Can Sell The Car at {}".format(output))
     else:
-        return render_template('index1.html')
+        return render_template('index.html')
 
 if __name__=="__main__":
     app.run(debug=True)
-
